d3969210f6826372a7df03054d378961d6d093cd
jacksparoow <kaushikweb6@gmail.com>
2021-10-27 14:32:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const myRoots: Routes =[
+  {path:'', component: HomeComponent, pathMatch:'full', canActivate:[AuthGuard]},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent,
+    HomeComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule, FormsModule, RouterModule.forRoot(myRoots, {enableTracing: true})
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
